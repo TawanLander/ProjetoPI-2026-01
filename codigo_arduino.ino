e4faceb2b6cc3d832fcb4f4feface295f6707a88
Tawan <tawan.moura@sptech.school>
2026-03-03 15:20:34 -0300

    Commit do código utilizado no arduino

---
+++ codigo_arduino.ino
@@ -0,0 +1,31 @@
+const int ValorAr = 550; // leitura no ar valor de calibração
+const int ValorAgua = 230; // leitura na ãgua valor de calibração
+
+int valorUmidadeSolo = 0;
+float porcentagemUmidade = 0;
+
+void setup() {
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  valorUmidadeSolo = analogRead(A5);
+
+  int faixa = ValorAr - ValorAgua;
+
+  int distancia = ValorAr - valorUmidadeSolo;
+
+  porcentagemUmidade = (float)distancia / faixa * 100.0;
+
+  if (porcentagemUmidade < 0) porcentagemUmidade = 0;
+  if (porcentagemUmidade > 100) porcentagemUmidade = 100;
+
+  Serial.print("Leitura bruta: ");
+  Serial.print(valorUmidadeSolo);
+  Serial.print(" | Umidade: ");
+  Serial.print(porcentagemUmidade);
+  Serial.print("%");
+
+  delay(1000);
+}

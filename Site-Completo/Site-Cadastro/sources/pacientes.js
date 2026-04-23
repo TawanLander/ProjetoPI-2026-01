125ed820121b70c0f3e57fcb368bb61005b9d295
TawanLander <tawan.moura@sptech.school>
2026-04-23 14:04:15 -0300

    Arrumando o institucional

--- Site-Completo/Site-Cadastro/sources/pacientes.js
+++ Site-Completo/Site-Cadastro/sources/pacientes.js
@@ -1,12 +1,15 @@
 const select = document.getElementById('select-pulseiras');
 const info = document.getElementById('info');
+const erro = document.getElementById('erro');
 
 function verificarPulseira() {
     let sValor = select.value;
 
     if(sValor != 'Pulseira 3'){
         info.classList.remove('sumir')
+        erro.classList.add('sumir');
     } else {
         info.classList.add('sumir')
+        erro.classList.remove('sumir')
     }
 }

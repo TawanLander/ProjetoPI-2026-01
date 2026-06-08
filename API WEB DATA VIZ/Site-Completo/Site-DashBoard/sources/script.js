94c35bf469b1bf30142551c2557802f639246fe0
TawanLander <tawan.moura@sptech.school>
2026-06-08 17:25:55 -0300

    ajuste

--- API WEB DATA VIZ/Site-Completo/Site-DashBoard/sources/script.js
+++ API WEB DATA VIZ/Site-Completo/Site-DashBoard/sources/script.js
@@ -86,17 +86,19 @@ async function obterDados(grafico, idSensor) {
     if (ultimaTemp) {
       const temperaturaAtual = parseFloat(ultimaTemp.temperatura);
 
-      document.getElementById("tempatual").innerHTML = `${temperaturaAtual === null || temperaturaAtual === undefined || isNaN(temperaturaAtual) ? 'Não Registrado' : temperaturaAtual}°`;
+      document.getElementById("tempatual").innerHTML = `${temperaturaAtual === null || temperaturaAtual === undefined || isNaN(temperaturaAtual) ? 0 : temperaturaAtual}°`;
 
-      document.getElementById("tempalta").innerHTML = `${ultimaTemp.tempMax === null || ultimaTemp.tempMax === undefined || ultimaTemp.tempMax === NaN ? 'Não Registrado' : ultimaTemp.tempMax}°`;
+      document.getElementById("tempalta").innerHTML = `${ultimaTemp.tempMax === null || ultimaTemp.tempMax === undefined || ultimaTemp.tempMax === NaN ? 0 : ultimaTemp.tempMax}°`;
 
-      document.getElementById("tempbaixa").innerHTML = `${ultimaTemp.tempMin === null || ultimaTemp.tempMin === undefined || ultimaTemp.tempMin === NaN ? 'Não Registrado' : ultimaTemp.tempMin}°`;
+      document.getElementById("tempbaixa").innerHTML = `${ultimaTemp.tempMin === null || ultimaTemp.tempMin === undefined || ultimaTemp.tempMin === NaN ? 0 : ultimaTemp.tempMin}°`;
 
       const estado = document.getElementById("estado");
 
       const divEstado = document.getElementById("div_tempAtual");
-
-      if (temperaturaAtual < 35) {
+      
+      if(isNaN(temperaturaAtual)){
+        estado.innerHTML = 'Não Registrado'
+      } else if (temperaturaAtual < 35) {
         estado.innerHTML = "Hipotermia";
 
         divEstado.style.backgroundColor = "#3b5bff";
